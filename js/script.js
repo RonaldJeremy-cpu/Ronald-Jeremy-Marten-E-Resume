31787525996fa19b632150631f31701c96ae717f
RonaldJeremy-cpu <74344889+RonaldJeremy-cpu@users.noreply.github.com>
2020-11-13 09:19:33 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function bigImg(x) {
+   x.style.height = "310px";
+   x.style.width = "230px";
+}
+
+function normalImg(x) {
+   x.style.height = "300px";
+   x.style.width = "220px";
+}
+
+alert ("Make sure your website size is 100% or full screen, to see better quality!");
